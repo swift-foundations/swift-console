4b9eac11ba21d31d11f97e0bbe419c84f6f9c413
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-03-20 12:45:39 +0100

    [MOD-006] Remove unused buffer-primitives dependency

--- Package.swift
+++ Package.swift
@@ -21,7 +21,6 @@ let package = Package(
         .package(path: "../swift-posix"),
         .package(path: "../../swift-ecma/swift-ecma-48"),
         .package(path: "../../swift-primitives/swift-terminal-primitives"),
-        .package(path: "../../swift-primitives/swift-buffer-primitives"),
         .package(path: "../../swift-primitives/swift-standard-library-extensions"),
     ],
     targets: [
@@ -32,7 +31,6 @@ let package = Package(
                 .product(name: "POSIX Kernel", package: "swift-posix", condition: .when(platforms: [.macOS, .iOS, .tvOS, .watchOS, .visionOS, .linux])),
                 .product(name: "ECMA 48", package: "swift-ecma-48"),
                 .product(name: "Terminal Input Primitives", package: "swift-terminal-primitives"),
-                .product(name: "Buffer Linear Inline Primitives", package: "swift-buffer-primitives"),
                 .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions"),
             ]
         ),
